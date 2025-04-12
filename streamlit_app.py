18b14172d36c799df641dedd8e9259d663a11814
Elisa <elisa3.riva@mail.polimi.it>
2025-04-12 14:58:21 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ page1 = st.Page('page1.py', title='Page 1')
 page2 = st.Page('page2.py', title='Page 2')
 
 pg = st.navigation([page1, page2])
-
 st.set_page_config(page_title='DV4S Exe2')
 
 # SELECT BOX
